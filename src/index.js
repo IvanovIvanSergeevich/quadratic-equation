2c8dad5f7b87a7c0e2246890c11f6bcefa5193e1
IvanovIvanSergeevich <ya.ivanov.i.s@gmail.com>
2018-02-27 23:41:13 +0300

    feature/index.js solve equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    equation = equation.replace(/\s/g,'');
+    equation = equation.replace(/\*x\^\d/g,' ');
+    equation = equation.replace(/\*x/g,' ');
+    var equationNmbrs = equation.split(' ');
+    var a = + equationNmbrs[0];
+    var b = + equationNmbrs[1];
+    var c = + equationNmbrs [2];
+    var discriminant = b*b - 4*a*c;
+
+    var result = [];
+    result[0]=Math.round((-b - Math.sqrt(discriminant))/(2*a));
+    result[1] =Math.round((-b + Math.sqrt(discriminant))/(2*a));
+
+    return result.sort((a,b)=>a-b);
 }
